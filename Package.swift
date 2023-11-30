3b8115c3f193cb4a73c7ab1144694fff8f019c88
Odin-AP <odin.song@adpopcorn.com>
2023-11-30 10:20:30 +0900

    [fix] file 위치 변경

--- Package.swift
+++ Package.swift
@@ -8,17 +8,19 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "APSSP_SPM_SDK_Sample",
-            targets: ["SSP_SPM_Sample"]),
+            name: "SSP_SPM",
+            targets: ["SSP_SPM"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         
-        .target(
-            name: "SSP_SPM_Sample"),
-        .testTarget(
-            name: "SSP_SPM_SampleTests",
-            dependencies: ["SSP_SPM_Sample"]),
+        .binaryTarget(name: "SSP_SPM", path: "Framework/AdPopcornSSP.xcframework"),
+        
+//        .target(
+//            name: "SSP_SPM_Sample"),
+//        .testTarget(
+//            name: "SSP_SPM_SampleTests",
+//            dependencies: ["SSP_SPM_Sample"]),
     ]
 )
