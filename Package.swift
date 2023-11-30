acae57fd916289a142ae6dfd9e2b77cbbb09c0f7
Odin-AP <odin.song@adpopcorn.com>
2023-11-30 10:00:09 +0900

    [add] xcframework 추가

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "APSSP_SPM_SDK_Sample",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "APSSP_SPM_SDK_Sample",
+            targets: ["SSP_SPM_Sample"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        
+        .target(
+            name: "SSP_SPM_Sample"),
+        .testTarget(
+            name: "SSP_SPM_SampleTests",
+            dependencies: ["SSP_SPM_Sample"]),
+        .binaryTarget(name: "SSP_SPM", path: "Framework/AdPopcornSSP.xcframework")
+    ]
+)
